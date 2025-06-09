13397c0d79ed51b10a5d757abf3c804beeccfe3b
adventuresoul <vinayme873@gmail.com>
2025-06-09 21:59:25 +0530

    added local storage

--- src/context/KanbanProvider.jsx
+++ src/context/KanbanProvider.jsx
@@ -1,18 +1,26 @@
 import { KanbanContext } from "./KanbanContext";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { KanbanReducer } from "./KanbanReducer";
 
+// initial tasks
+const initialTasks = [];
+
+// get saved tasks if present
+const getInitialDataFromLocalStorage = () => {
+    const saved = localStorage.getItem("KanbanBoard");
+    return saved ? JSON.parse(saved) : initialTasks;
+}
 
 export const KanbanProvider = ({ children }) => {
-    const initialTasks = [];
+    
 
-    const [ boardData, dispatch ] = useReducer(KanbanReducer, initialTasks);
+    const [ boardData, dispatch ] = useReducer(KanbanReducer, initialTasks, getInitialDataFromLocalStorage);
 
     const addTask = (task) => dispatch({ type: "ADD_TASK", payload: task });
     const deleteTask = (id) => dispatch({ type: "DELETE_TASK", payload: {id} });
     const editTask = (id, updatedTask) => dispatch({ type: "EDIT_TASK", payload: {id, updatedTask} });
     const moveTask = (id, toColumn) => dispatch({ type: "MOVE_TASK", payload: {id, toColumn} });
-    
+
 
     const value = {
         boardData,
@@ -22,6 +30,11 @@ export const KanbanProvider = ({ children }) => {
         editTask,
         moveTask
     };
+
+    // on any state change sync the localSavedData
+    useEffect(() => {
+        localStorage.setItem("KanbanBoard", JSON.stringify(boardData))
+    }, [boardData]);
 
     return (
         <KanbanContext.Provider value={value}>
